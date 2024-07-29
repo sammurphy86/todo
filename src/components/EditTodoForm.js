dca58d825da96a7e1ab7a41f6349febf81c7e144
Sam Murphy <sammurphy86@gmail.com>
2024-07-29 14:24:56 +1000

    29/7/2024

---
+++ src/components/EditTodoForm.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const EditTodoFrom = () => {
+  return <div>EditTodoFrom</div>;
+};
---
+++ src/components/TodoWrapper.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { TodoForm } from "./TodoForm";
+import { Todo } from "./Todo";
+import { v4 as uuidv4 } from "uuid";
+uuidv4();
+
+export const TodoWrapper = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+    console.log(todos);
+  };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  return (
+    <div className="TodoWrapper">
+      <h1>ToDo List!</h1>
+      <TodoForm addTodo={addTodo} />
+      {todos.map((todo, index) => (
+        <Todo task={todo} key={index} toggleComplete={toggleComplete} />
+      ))}
+    </div>
+  );
+};
